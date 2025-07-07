3fe8d63f11c167b0eafb5cfd86b13405dd3d9389
anshu <astitvabandil@gmail.com>
2025-07-07 16:31:01 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Note Taking App Backend Running');
+  res.send('Note Taking App new Backend Running');
 });
 
 createTables();
